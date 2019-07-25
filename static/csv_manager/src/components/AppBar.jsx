16e9e9d7f298fe0b9e2057f3f568d9943dbc6399
khaledboka <bokae97@gmail.com>
2019-07-25 10:31:15 +0200

    App Bar

---
+++ static/csv_manager/src/components/AppBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    height: '64px'
+  },
+  appBar: {
+    backgroundColor: '#2196f3'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            CSV Manager
+          </Typography>
+          <Button color="inherit">Upload Layer</Button>
+          <Button color="inherit">Data Manager</Button>
+          <Button color="inherit">Home</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
