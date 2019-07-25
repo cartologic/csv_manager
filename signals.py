4a0aca8c391ac148bc65ef6b0741adfda710fad7
khaledboka <bokae97@gmail.com>
2019-07-25 10:10:41 +0200

    django delete signal

---
+++ signals.py
@@ -0,0 +1,14 @@
+from .models import CSVUpload
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import os
+import shutil
+
+
+@receiver(post_delete, sender=CSVUpload)
+def delete_csv_dir(sender, instance, **kwargs):
+    file_path = instance.csv_file.path
+    if os.path.exists(file_path):
+        dir_path = os.path.dirname(file_path)
+        # rm files csv, vrt ...
+        shutil.rmtree(dir_path, ignore_errors=True)
