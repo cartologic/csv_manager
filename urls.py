93b147eda978b61c02c0106692c29a35f2eb6dd7
khaledboka <bokae97@gmail.com>
2019-07-25 10:08:38 +0200

    all urls

--- urls.py
+++ urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from . import views, APP_NAME
+from tastypie.api import Api
+from .api import CSVUploadResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(CSVUploadResource())
 
 urlpatterns = [
    url(r'^$', views.index, name='%s.index' % APP_NAME),
    url(r'^upload/', views.upload, name='%s.upload' % APP_NAME),
+   url(r'^publish/', views.publish, name='%s.upload' % APP_NAME),
+   url(r'^api/', include(v1_api.urls)),
 ]
