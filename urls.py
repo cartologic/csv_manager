fe24d4dbe4907860e52fefc3abb3a4e941894075
khaledboka <bokae97@gmail.com>
2019-07-25 13:18:53 +0200

    changed API name

--- urls.py
+++ urls.py
@@ -4,12 +4,12 @@ from tastypie.api import Api
 from . import views, APP_NAME
 from .api import CSVUploadResource
 
-v1_api = Api(api_name='v1')
+v1_api = Api(api_name='csv_api')
 v1_api.register(CSVUploadResource())
 
 urlpatterns = [
     url(r'^$', views.index, name='%s.index' % APP_NAME),
     url(r'^upload/', views.upload, name='%s.upload' % APP_NAME),
-    url(r'^publish/', views.publish, name='%s.upload' % APP_NAME),
+    url(r'^publish/', views.publish, name='%s.publish' % APP_NAME),
     url(r'^api/', include(v1_api.urls)),
 ]
