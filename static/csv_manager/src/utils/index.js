043ab12543811c049f66faa4011051eca1ba055e
khaledboka <bokae97@gmail.com>
2019-11-07 12:05:37 +0200

    separate validation functions into utils

--- static/csv_manager/src/utils/index.js
+++ static/csv_manager/src/utils/index.js
@@ -5,3 +5,12 @@ export const getCRSFToken = () => {
     }
     return csrfToken
 };
+
+export const validateFieldName = (fieldName) => {
+    return fieldName && fieldName.length > 0
+}
+
+export const validateTableName = (tableName) => {
+    let re = /^[a-z0-9_]{1,63}$/
+    return tableName && re.test(tableName)
+}
