2eaa408ee8b7df44e5c597223594039dc6f8969c
Khaled Boka <bokae97@gmail.com>
2020-12-15 13:28:02 +0200

    tasty pie dehydrate field names and better user

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
+import json
 import os
 
 from django.conf import settings
+from geonode.api.api import ProfileResource
+from tastypie import fields
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.fields import ListField
 from tastypie.resources import ModelResource
@@ -11,6 +14,8 @@ from .models import CSVUpload
 
 class CSVUploadResource(ModelResource):
     fields_names = ListField()
+    # user = fields.ForeignKey(ProfileResource, 'user', full=True, verbose_name='owner')
+    user = fields.DictField()
 
     class Meta:
         queryset = CSVUpload.objects.all()
@@ -32,6 +37,12 @@ class CSVUploadResource(ModelResource):
         return super(CSVUploadResource, self).get_object_list(request).filter(user=request.user)
 
     def dehydrate_fields_names(self, bundle):
-        csv_path = bundle.obj.csv_file.path
-        full_path = os.path.join(settings.MEDIA_ROOT, csv_path)
-        return get_field_names(full_path)
+        fields_names = json.loads(bundle.obj.fields_names)
+        return fields_names
+
+    def dehydrate_user(self, bundle):
+        user = bundle.obj.user
+        return {
+            'username': user.username,
+            'id': user.id,
+        }
